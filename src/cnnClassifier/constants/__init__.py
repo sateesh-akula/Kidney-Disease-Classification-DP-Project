6f762614669fae85c73e31973181468941f2d90d
sateesh-akula <sateeshakula1996@gmail.com>
2024-07-03 20:26:36 +0530

    Added Data Ingestion

---
+++ src/cnnClassifier/constants/__init__.py
@@ -0,0 +1,6 @@
+# creating constants folder for reading config.yaml and params.yaml
+
+from pathlib import Path
+
+CONFIG_FILE_PATH = Path("config/config.yaml")
+PARAMS_FILE_PATH = Path("params.yaml")
